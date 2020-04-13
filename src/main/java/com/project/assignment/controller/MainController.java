fd3b5cc6ad59735430ce5f354f79d54ccbb3a1d9
Rohan Shrimal <rohanshrimal@rshrimal-mac.local>
2020-04-13 15:24:01 +0530

    Status Codes added in GET and POST responses

--- src/main/java/com/project/assignment/controller/MainController.java
+++ src/main/java/com/project/assignment/controller/MainController.java
@@ -1,12 +1,25 @@
 package com.project.assignment.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import com.project.assignment.model.UserModel;
 import com.project.assignment.service.UserService;
@@ -18,14 +31,36 @@ public class MainController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+    private ObjectMapper mapper;
+	
+	@Value("${message.user.created}")
+	private String usercreatedMessage;
+	
+
 	@GetMapping("/user/{id}")
-	public UserModel getUser(@PathVariable Long id) {
-		return userService.getUser(id);
+	public ResponseEntity<UserModel> getUser(@PathVariable Long id) {
+		return ResponseEntity.ok().body(userService.getUser(id));
 	}
 	
 	@PostMapping("/user")
-	public UserModel addUser(@RequestBody UserModel userModel) {
-		return userService.addUser(userModel);
+	public ResponseEntity<String> addUser(@RequestBody UserModel userModel) throws JsonProcessingException, URISyntaxException {
+		ResponseEntity<String> response = null;
+        JsonNode responseJson = mapper.createObjectNode();
+        
+        UserModel createdUser = userService.addUser(userModel);
+        
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+    			.buildAndExpand(createdUser.getId()).toUri();
+        
+        ((ObjectNode) responseJson).put("status", HttpStatus.CREATED.value());
+        ((ObjectNode) responseJson).put("message", usercreatedMessage);
+        response = ResponseEntity.created(location)
+                    .body(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseJson));
+        
+        
+
+        return response;
 	}
 
 }
