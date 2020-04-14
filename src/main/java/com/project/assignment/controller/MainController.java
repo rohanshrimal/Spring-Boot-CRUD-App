12802acc4bbcf55b10d5846158d6a17032792747
Rohit Jangid <Rohit.Jangid@AlticeUSA.com>
2020-04-14 21:08:41 +0530

    Put and Delete User Code

--- src/main/java/com/project/assignment/controller/MainController.java
+++ src/main/java/com/project/assignment/controller/MainController.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,42 +28,51 @@ import com.project.assignment.service.UserService;
 @RestController
 @RequestMapping
 public class MainController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
-    private ObjectMapper mapper;
-	
+	private ObjectMapper mapper;
+
 	@Value("${message.user.created}")
 	private String usercreatedMessage;
-	
 
 	@GetMapping("/user/{id}")
 	public ResponseEntity<UserModel> getUser(@PathVariable Long id) {
 		return ResponseEntity.ok().body(userService.getUser(id));
 	}
-	
+
 	@PostMapping("/user")
-	public ResponseEntity<String> addUser(@RequestBody UserModel userModel) throws JsonProcessingException, URISyntaxException {
+	public ResponseEntity<String> addUser(@RequestBody UserModel userModel)
+			throws JsonProcessingException, URISyntaxException {
 		ResponseEntity<String> response = null;
 
-        userModel.setId(null);
-        UserModel createdUser = userService.addUser(userModel);
-        
-        URI location = ServletUriComponentsBuilder
-        					.fromCurrentRequest()
-							.path("/{id}")
-							.buildAndExpand(createdUser.getId()).toUri();
-        
-        JsonNode responseJson = mapper.createObjectNode();
-        ((ObjectNode) responseJson).put("status", HttpStatus.CREATED.value());
-        ((ObjectNode) responseJson).put("message", usercreatedMessage);
-        response = ResponseEntity
-        			.created(location)
-                    .body(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseJson));
-        
-        return response;
+		userModel.setId(null);
+		UserModel createdUser = userService.addUser(userModel);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(createdUser.getId()).toUri();
+
+		JsonNode responseJson = mapper.createObjectNode();
+		((ObjectNode) responseJson).put("status", HttpStatus.CREATED.value());
+		((ObjectNode) responseJson).put("message", usercreatedMessage);
+		response = ResponseEntity.created(location)
+				.body(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseJson));
+
+		return response;
+	}
+
+	@DeleteMapping("/user/{id}")
+	public void deleteUser(@PathVariable long id) {
+		userService.deleteUser(id);
+	}
+
+	@PutMapping("/user/{id}")
+	public ResponseEntity<UserModel> updateUser(@RequestBody UserModel userModel, @PathVariable long id) {
+
+		return userService.updateUser(id, userModel);
+
 	}
 
 }
--- src/main/java/com/project/assignment/service/UserService.java
+++ src/main/java/com/project/assignment/service/UserService.java
@@ -1,6 +1,9 @@
 package com.project.assignment.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.project.assignment.model.UserModel;
@@ -11,12 +14,30 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
-	
+
 	public UserModel addUser(UserModel userModel) {
-		return userRepository.save(userModel);	
+		return userRepository.save(userModel);
 	}
 
 	public UserModel getUser(Long id) {
 		return userRepository.getUserById(id);
+	}
+
+	public void deleteUser(long id) {
+		userRepository.deleteById(id);
+
+	}
+
+	public ResponseEntity<UserModel> updateUser(long id, UserModel userModel) {
+		Optional<UserModel> userOptional = userRepository.findById(id);
+
+		if (!userOptional.isPresent())
+			return ResponseEntity.notFound().build();
+
+		userModel.setId(id);
+
+		userRepository.save(userModel);
+
+		return ResponseEntity.noContent().build();
 	}
 }
